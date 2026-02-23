744b31b2ba361e6f5103cd0b60174296b8903658
Андрей Мороз <andrejmoroz@Noutbuk-Andrej.local>
2026-02-23 16:43:44 +0200

    feature(EPIC-03)-TASK-1

--- src/graphql/schema/queries/boards.ts
+++ src/graphql/schema/queries/boards.ts
@@ -1,12 +1,21 @@
 export const boardQueries = `
-    extend type Query {
-        board(id: ID!): Board
-        boards(
-            first: Int
-            after: String
-            last: Int
-            before: String
-            visibility: BoardVisibility
-        ): BoardConnection!
-    }
+  enum BoardSortBy {
+    NAME
+    CREATED_AT
+    UPDATED_AT
+  }
+
+  extend type Query {
+    board(id: ID!): Board
+
+    boards(
+      sortBy: BoardSortBy
+      sortOrder: SortOrder
+      visibility: BoardVisibility
+      first: Int
+      after: String
+      last: Int
+      before: String
+    ): BoardConnection!
+  }
 `;
